1a42f07a7b8e52a739fe6cb97c67eb4ff2b933a0
catwarm <turtlewang123@hotmail.com>
2025-03-02 01:22:24 +0000

    main2 good

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-	
+	int n,k,mnt;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		if (x%10 == k)
+		{
+			mnt += x;
+		}
+	}
+	cout << mnt;
   	return 0;
 }
